8dd12f9bb680d411626ce135ffdc8f5144f3ede6
Andrei Legaspi <117955501+loremIPSAGEL@users.noreply.github.com>
2023-02-09 15:02:32 +0800

    Delete practice_folder directory

--- practice_folder/user.js
+++
@@ -1,8 +0,0 @@
-  const manageAccountsIcon = document.querySelector('#manage-accounts-icon');
-  const userAccountInterface = document.querySelector('.user-account-interface');
-
-  manageAccountsIcon.addEventListener('click', function() {
-    userAccountInterface.style.display = (userAccountInterface.style.display === 'none') ? 'block' : 'none';
-  });
-
-
